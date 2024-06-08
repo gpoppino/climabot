35e70400f92818bef670ac044942308560596d61
Geronimo Poppino <gpoppino@outlook.com>
2024-06-08 16:26:44 -0300

    Use new openAI API

--- climabot/cogs/openai.py
+++ climabot/cogs/openai.py
@@ -1,5 +1,5 @@
 import os
-import openai
+from openai import AsyncOpenAI
 
 from discord.ext import commands
 
@@ -8,7 +8,9 @@ class OpenAIGPT(commands.Cog):
 
     def __init__(self, bot):
         # Load your API key from an environment variable or secret management service
-        openai.api_key = os.getenv("OPENAI_API_KEY")
+        self.__client = AsyncOpenAI(
+            api_key=os.getenv("OPENAI_API_KEY")
+        )
         self.__bot = bot
 
     @commands.command(name="gpt", help="Generates text using OpenAI GPT-3.5 Turbo")
@@ -18,10 +20,9 @@ class OpenAIGPT(commands.Cog):
 
         message = ' '.join(args).strip()
         async with ctx.typing():
-            response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                                    messages=[
-                                                        {"role": "system", "content": "You are a helpful assistant."},
-                                                        {"role": "user", "content": message}
-                                                    ]
-                                                    )
-        await ctx.send(response['choices'][0]['message']['content'])
+            response = await self.__client.chat.completions.create(model="gpt-3.5-turbo",
+                                                                   messages=[{"role": "system",
+                                                                              "content": "You are a helpful assistant"},
+                                                                             {"role": "user", "content": message}]
+                                                                   )
+        await ctx.send(response.choices[0].message.content)
